f330d7f978ad0453c682b5e33d5c2c83c528fc2b
minthihalwin <minthihalwin.dev@gmail.com>
2025-03-19 00:33:26 +1300

    Feat: add Tailwind CSS configuration for responsive design

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    screens: {
+      'xs': '480px',      // Extra small screens
+      'sm': '640px',      // Small screens (default)
+      'md': '768px',      // Medium screens (default)
+      'lg': '1024px',     // Large screens (default)
+      'xl': '1280px',     // Extra large screens (default)
+      '2xl': '1536px',    // 2X Extra large screens (default)
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
